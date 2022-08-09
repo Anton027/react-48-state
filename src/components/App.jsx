b435f8d2c96d78f833160ed73c5cc1626b0690a0
Anton Kutsim <kutsimanton@gmail.com>
2022-08-09 14:04:50 +0300

    prepere comit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { GlobalStyle } from './GlobalStyle';
+import { StickerList } from './StickerList';
+import stickers from '../stickers.json';
+import { Component } from 'react';
+
+const SelectedLabel = ({ text }) => {
+  return <h1>{text}</h1>;
 };
+
+export class App extends Component {
+  state = {
+    selectedStickerLabel: null,
+  };
+
+  selectStickerLabel = label => {
+    this.setState({
+      selectedStickerLabel: label,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <SelectedLabel text={this.state.selectedStickerLabel} />
+        <StickerList stickers={stickers} onSelect={this.selectStickerLabel} />
+        <GlobalStyle />
+      </>
+    );
+  }
+}
